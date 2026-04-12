e626d026efc086dc1f9f350d41eb226b5893350b
Clesiaroberto <clesiaroberto@yahoo.com>
2026-04-12 21:53:46 +0200

    Added count items on cart and disab;ed and changed button of plants when it is clicked

--- src/CartItem.jsx
+++ src/CartItem.jsx
@@ -34,7 +34,7 @@ const CartItem = ({ onContinueShopping }) => {
 
   return (
     <div className="cart-container">
-      <h2 style={{ color: 'black' }}>Total Cart Amount: ${calculateTotalAmount()}</h2>
+      <h2 style={{ color: 'black', fontSize:'20px' }}>Total Cart Amount: ${calculateTotalAmount()}</h2>
       <div>
         {cart.map(item => (
           <div className="cart-item" key={item.name}>
@@ -55,7 +55,7 @@ const CartItem = ({ onContinueShopping }) => {
       </div>
       <div style={{ marginTop: '20px', color: 'black' }} className='total_cart_amount'></div>
       <div className="continue_shopping_btn">
-        <button className="get-started-button" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
+        <button className="get-started-button1" onClick={(e) => handleContinueShopping(e)}>Continue Shopping</button>
         <br />
         <button className="get-started-button1">Checkout</button>
       </div>
--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -1,6 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './ProductList.css'
 import CartItem from './CartItem';
+
+
 function ProductList({ onHomeClick }) {
     const [showCart, setShowCart] = useState(false);
     const [showPlants, setShowPlants] = useState(false); // State to control the visibility of the About Us page
@@ -233,7 +235,7 @@ function ProductList({ onHomeClick }) {
         alignItems: 'center',
         backgroundColor:'',
         width: '100vw',
-        position:'fixed',
+        position:'absolute',
 
     }
     const styleA = {
@@ -261,13 +263,26 @@ function ProductList({ onHomeClick }) {
         e.preventDefault();
         setShowCart(false);
     };
+
+    const [cartItems, setCartItems] = useState(0);
+    const handleButtonClick = (event) =>{
+        event.target.style.backgroundColor = "grey";
+        event.target.innerText = "Added to Cart";
+        event.target.disabled = "true";
+
+         setCartItems(cartItems+1)
+
+    }
+
+
     return (
         <div>
             <div className="navbar" style={styleObj}>
                 <div className="tag">
                     <div className="luxury">
-                        <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
+                        
                         <a href="/" onClick={(e) => handleHomeClick(e)}>
+                        <img src="https://cdn.pixabay.com/photo/2020/08/05/13/12/eco-5465432_1280.png" alt="" />
                             <div style={{marginLeft: '10px', display:'inline-block'}}>
                                 <h3 style={{ color: 'white' }}>Paradise Nursery</h3>
                                 <i style={{ color: 'white' }}>Where Green Meets Serenity</i>
@@ -285,7 +300,10 @@ function ProductList({ onHomeClick }) {
                         <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
                          <h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68">
                          <rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle>
-                         <circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg>
+                         <circle cx="184" cy="216" r="12"></circle>
+                         <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path>
+                         </svg>
+                         <p style={{position:'absolute', fontSize:'16px', margin:'25px 28px'}}>{cartItems}</p>
                          </h1></a></div>
                 </div>
             </div>
@@ -305,7 +323,7 @@ function ProductList({ onHomeClick }) {
                                 <img className='product-image' src={plant.image}/>
                                 <div className='product-price'>{plant.cost}</div>
                                 <div className='product-list'>{plant.description}</div>
-                                <div className='product-button'>Add to Cart</div>
+                                <button className='product-button' onClick={handleButtonClick}>Add to Cart</button>
                             </div>
                             
                         ))}
